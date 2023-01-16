ab8d84bfeca8c034ec701b2a6bc3ddc625bbae14
chandhan619 <62613173+chandhan619@users.noreply.github.com>
2023-01-16 19:01:43 +0530

    Commit 2

---
+++ SCApi/src/main/java/com/chandhan/SC_API/App.java
@@ -0,0 +1,16 @@
+package com.chandhan.SC_API;
+
+public class App {
+	
+	
+	  private static final String url = "";
+	  private static final String key = "";
+	  private static final String secret = "";
+	  private static final boolean debug = false;
+	  private static final boolean setProxy = false;
+public static void main(String[] args) {
+
+	
+	APIRequestHandler handler = new APIRequestHandler(url, key,secret, debug, setProxy);
+}
+}
